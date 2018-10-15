e243250912bb730f146461e1cd8be08e0a9afbad
Bo <clouculum@gmail.com>
2018-10-14 21:57:04 -0700

    List all question formats

--- FirstQuizApp/index.js
+++ FirstQuizApp/index.js
@@ -1,1 +1,82 @@
 $('body').append('<div>Iam Here</div>');
+{
+    number: 1,
+    text: `The process of burning of municipal solid waste at high temperature is called`,
+    ans1: `Incineration`, 
+    ans2: `Composting`, 
+    ans3: `Land filing`, 
+    ans4: `Shredding`
+  },
+  { 
+    number: 2,
+    text: ``,
+    ans1: ``,
+    ans2: ``, 
+    ans3: ``, 
+    ans4: ``
+  }, 
+  { 
+    number: 3,
+    text: ``,
+    ans1: ``,
+    ans2: ``, 
+    ans3: ``, 
+    ans4: ``
+  }, 
+  { 
+    number: 4,
+    text: ``,
+    ans1: ``,
+    ans2: ``, 
+    ans3: ``, 
+    ans4: ``
+  }, 
+  { 
+    number: 5,
+    text: ``,
+    ans1: ``,
+    ans2: ``, 
+    ans3: ``, 
+    ans4: ``
+  }, 
+  { 
+    number: 6,
+    text: ``,
+    ans1: ``,
+    ans2: ``, 
+    ans3: ``, 
+    ans4: ``
+  }, 
+  { 
+    number: 7,
+    text: ``,
+    ans1: ``,
+    ans2: ``, 
+    ans3: ``, 
+    ans4: ``
+  }, 
+  { 
+    number: 8,
+    text: ``,
+    ans1: ``,
+    ans2: ``, 
+    ans3: ``, 
+    ans4: ``
+  }, 
+  { 
+    number: 9,
+    text: ``,
+    ans1: ``,
+    ans2: ``, 
+    ans3: ``, 
+    ans4: ``
+  }, 
+  { 
+    number: 10,
+    text: ``,
+    ans1: ``,
+    ans2: ``, 
+    ans3: ``, 
+    ans4: ``
+  }, 
+
