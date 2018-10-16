f1f67d8e363f1f708f59e9aec43cbc841ed477be
bo <clouculum@gmail.com>
2018-10-15 23:57:44 -0700

    Question array added.

--- FirstQuizApp/index.js
+++ FirstQuizApp/index.js
@@ -9,74 +9,74 @@ $('body').append('<div>Iam Here</div>');
   },
   { 
     number: 2,
-    text: ``,
-    ans1: ``,
-    ans2: ``, 
-    ans3: ``, 
-    ans4: ``
+    text: `Which of the following is a biodegradable waste?`,
+    ans1: `Polythene bags`,
+    ans2: `Polythene bags`, 
+    ans3: `Food waste`, 
+    ans4: `Paper`
   }, 
   { 
     number: 3,
-    text: ``,
-    ans1: ``,
-    ans2: ``, 
-    ans3: ``, 
-    ans4: ``
+    text: `In which method of disposal of municipal solid waste, the waste is dumped in the soil?`,
+    ans1: `Incineration`,
+    ans2: `Composting`, 
+    ans3: `Land filing`, 
+    ans4: `Shredding`
   }, 
   { 
     number: 4,
-    text: ``,
-    ans1: ``,
-    ans2: ``, 
-    ans3: ``, 
-    ans4: ``
+    text: `Which of the following is correct regarding disposal of waste by land filling?`,
+    ans1: `Economical method`,
+    ans2: `Preferred in low lying areas`, 
+    ans3: `Foul gases are not produced`, 
+    ans4: `Separation of different types of waste not required`
   }, 
   { 
     number: 5,
-    text: ``,
-    ans1: ``,
-    ans2: ``, 
-    ans3: ``, 
-    ans4: ``
+    text: `The density of ash produced in the municipal solid waste is`,
+    ans1: `100 kg/m3`,
+    ans2: `450 kg/m3`, 
+    ans3: `700 kg/m3`, 
+    ans4: `1000 kg/m3`
   }, 
   { 
     number: 6,
-    text: ``,
-    ans1: ``,
-    ans2: ``, 
-    ans3: ``, 
-    ans4: ``
+    text: `The process of decomposition of biodegradable solid waste by earthworms is called`,
+    ans1: `Land fills`,
+    ans2: `Shredding`, 
+    ans3: `Vermi-composting`, 
+    ans4: `Composting`
   }, 
   { 
     number: 7,
-    text: ``,
-    ans1: ``,
-    ans2: ``, 
-    ans3: ``, 
-    ans4: ``
+    text: `The waste produced in cotton mills are`,
+    ans1: `Municipal solid waste`,
+    ans2: `Non biodegradable waste`, 
+    ans3: `Hazardous waste`, 
+    ans4: `Non hazardous waste`
   }, 
   { 
     number: 8,
-    text: ``,
-    ans1: ``,
-    ans2: ``, 
-    ans3: ``, 
-    ans4: ``
+    text: `Which of the following is not the land filling method?`,
+    ans1: `Bangalore method`,
+    ans2: `Area method`, 
+    ans3: `Depression method`, 
+    ans4: `Trench method`
   }, 
   { 
     number: 9,
-    text: ``,
-    ans1: ``,
-    ans2: ``, 
-    ans3: ``, 
-    ans4: ``
+    text: `______ is a liquid that passes through solid waste and extract suspended impurities from it.`,
+    ans1: `Leachate`,
+    ans2: `Sludge`, 
+    ans3: `Distilled water`, 
+    ans4: `Municipal waste`
   }, 
   { 
     number: 10,
-    text: ``,
-    ans1: ``,
-    ans2: ``, 
-    ans3: ``, 
-    ans4: ``
+    text: `Which of the following is not the municipal solid waste?`,
+    ans1: `Radioactive substance`,
+    ans2: `Ashes`, 
+    ans3: `Food waste`, 
+    ans4: `Rubbish`
   }, 
 
