c7a9f6aff2ff60d7ac40edb1cd8c6e80c0b29952
Bo <clouculum@gmail.com>
2018-12-15 21:12:48 -0700

    update

--- project_BookSurf/index.js
+++ project_BookSurf/index.js
@@ -4,9 +4,9 @@ function getDataFromApi(searchTerm, callback) {
   const query = {
     fields: 'items',
     key: 'AIzaSyAouaY0zJ3VYlPM-iNeww5hQaUWEPAfOYM',
-    q: `${searchTerm}`,
+    q: `${searchTerm} , subject`,
     orderBy: 'relevance',
-    maxResults: 28,
+    maxResults: 40,
   }
   $.getJSON(GBOOK_SEARCH_URL, query, callback);
 }
@@ -18,8 +18,8 @@ function renderResult(result) {
       <div class="flex-container">
         <div class="col-3">
           <img src="${result.volumeInfo.imageLinks.thumbnail}" alt="Book thumbnail image">
-          <h3><span class="js-book-title">${result.volumeInfo.title}</span></h3>
-          <h3 class="js-book-author">${result.volumeInfo.authors}</h3>
+          <h3 class="js-book-title" title="${result.volumeInfo.title}">${result.volumeInfo.title}</h3>
+          <h3 class="js-book-author" title="${result.volumeInfo.authors}">${result.volumeInfo.authors}</h3>
         </div>
       </div>
       `
@@ -28,15 +28,12 @@ function renderResult(result) {
 
 function displayBookSearchData(data) {
   const bookList = [];
-  const apiImg = ${result.volumeInfo.imageLinks.thumbnail};
-
+//Iterate book information from API
   for (let i = 0; i < data.items.length; i++) {
       bookList.push(renderResult(data.items[i]));
   }
-  if (apiImg == undefined) {
-    $('.col-3').add
-  }
-  $('.js-search-results').html(bookList.join(""));
+
+  $('.js-search-results').html(bookList);
 }
 
 function watchSubmit() {
@@ -44,7 +41,9 @@ function watchSubmit() {
         function (event) {
             event.preventDefault();
             const query = $('.js-query').val();
+            const queryClr = $(event.currentTarget).find(".js-query")
             getDataFromApi(query, displayBookSearchData);
+            queryClr.val("");
         });
         
 }
